f9e716e822d4ce8128a786b163d735eb17c91dbf
felipe <fearlimasi@gmail.com>
2023-03-05 10:50:30 -0300

    feat: Find servers by host

--- app/app.go
+++ app/app.go
@@ -14,17 +14,25 @@ func Generate() *cli.App {
 	app.Name = "Command line application"
 	app.Usage = "Find ip and names on internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com.br",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Search for IP addresses on the internet.",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "google.com.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Search for IP addresses by host on the internet.",
+			Flags:  flags,
 			Action: findIps,
+		},
+		{
+			Name:   "servers",
+			Usage:  "Search for servers by host on the internet.",
+			Flags:  flags,
+			Action: findServers,
 		},
 	}
 
@@ -43,4 +51,19 @@ func findIps(c *cli.Context) {
 	for _, ip := range ips {
 		fmt.Println(ip)
 	}
+}
+
+func findServers(c *cli.Context) {
+	host := c.String("host")
+
+	hosts, err := net.LookupNS(host)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, host := range hosts {
+		fmt.Println(host.Host)
+	}
+
 }
